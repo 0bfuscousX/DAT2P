87aa486c0afa9b62d0bf196090a537782a4dd18f
0bfuscousX <lvlim@student.apc.edu.ph>
2023-03-10 08:41:30 +0800

    sign-up

--- ADMIN/php-signup.php
+++ ADMIN/php-signup.php
@@ -49,6 +49,28 @@
     </fielsdet>
     </form>
 
+    <?php
+	// define variables and set to empty values
+    $Name = $username = $email = $password = $number = $date = $gender = "";
+
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	  $Name = test_input($_POST["name"]);
+	  $username = test_input($_POST["username"]);
+	  $email = test_input($_POST["email"]);
+	  $password = test_input($_POST["password"]);
+      $number = test_input($_POST["number"]);
+	  $date = test_input($_POST["date"]);
+	  $gender = test_input($_POST["gender"]);
+	}
+
+	function test_input($data) {
+	  $data = trim($data);
+	  $data = stripslashes($data);
+	  $data = htmlspecialchars($data);
+	  return $data;
+	}
+	?>
+
 <!--insert and recording of data-->
 
 	<?php
